ee1fcbeddc27b038ee9071245b2d138f5d968a2e
Sophia Yougin <sophiya273@gmail.com>
2020-10-07 20:09:55 +0530

    Updates

---
+++ precache-manifest.2126d6ce45509838538d8e7b7eea5a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d099a7524e7ad5c9b4f5d44404243d",
+    "url": "/shoppingCart/index.html"
+  },
+  {
+    "revision": "db43e4e49fdd94c8634f",
+    "url": "/shoppingCart/static/css/main.4e2f6661.chunk.css"
+  },
+  {
+    "revision": "b6f83f153c7f13e4f60f",
+    "url": "/shoppingCart/static/js/2.1bd0fd1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppingCart/static/js/2.1bd0fd1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db43e4e49fdd94c8634f",
+    "url": "/shoppingCart/static/js/main.60792d0a.chunk.js"
+  },
+  {
+    "revision": "faec1e8c096291b61bbc",
+    "url": "/shoppingCart/static/js/runtime-main.1a2920fc.js"
+  }
+]);
